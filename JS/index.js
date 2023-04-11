82899daf29a04ac07bada823f88ce7a3d804e285
Brittney Brinson <brittneyc95@outlook.com>
2023-04-10 21:04:48 -0400

    Got the loading screen video to transition as well

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,6 @@
 setTimeout(function() {
     document.body.style.backgroundImage = "url('/IMG/fake-data.svg')";
+    document.getElementsByClassName('video-container')[0].style.display = 'none';
 }, 8000);
 
 // the body element is initially set to have a background image of the loading page using the background-image property in CSS. The transition property is also used to add a smooth transition effect when the background image changes.
