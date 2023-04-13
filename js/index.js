60231def6183a75c0e09aa594f4dae47697e6d6b
Brittney Brinson <brittneyc95@outlook.com>
2023-04-13 00:36:45 -0400

    Almost done with the input box, just need to connect the search features

--- js/index.js
+++ js/index.js
@@ -3,23 +3,24 @@ setTimeout(function() {
     document.getElementsByClassName('video-container')[0].style.display = 'none';
     document.getElementsByClassName('glass-container')[0].style.display = 'flex';
     document.getElementsByClassName('location-container')[0].style.display = 'flex';
+    document.getElementsByClassName('button-container')[0].style.display = 'inline-block';
 }, 8000);
 // the body element is initially set to have a background image of the loading page using the background-image property in CSS. The transition property is also used to add a smooth transition effect when the background image changes.
 //
 //     JavaScript is used to trigger the image change after an 8-second delay using the setTimeout function. The setTimeout function takes a callback function as its first argument, which sets the backgroundImage property of the body element to the fake data image after 8 seconds, effectively changing the background image.
 //Next I need to use AJAX to make the call(request) to the API and get the data. I will use the jQuery library to make the AJAX call.
 
-function makeAjaxCall(){
-    $.ajax({
-        url: 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=6f1f6d5b6b5c7b5f2b2e7d2f6c5f7d1e',
-        type: 'GET',
-        dataType: 'json',
-        success: function(data){
-            console.log(data);
-        },
-        error: function(error){
-            console.log(error);
-        }
-    });
-}
-makeAjaxCall();
+// function makeAjaxCall(){
+//     $.ajax({
+//         url: 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=6f1f6d5b6b5c7b5f2b2e7d2f6c5f7d1e',
+//         type: 'GET',
+//         dataType: 'json',
+//         success: function(data){
+//             console.log(data);
+//         },
+//         error: function(error){
+//             console.log(error);
+//         }
+//     });
+// }
+// makeAjaxCall();
