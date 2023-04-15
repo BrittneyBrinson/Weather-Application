d3671f256d47dbf1900aa447332d47ed8914d358
Brittney Brinson <brittneyc95@outlook.com>
2023-04-15 02:43:28 -0400

    Did some cosmetic changes and now I have to re-position elements in the navbar

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 setTimeout(function() {
-    document.body.style.backgroundImage = "url('/IMG/sky-blue.svg')";
-    document.getElementsByClassName('video-container')[0].style.display = 'none';
+    document.body.style.backgroundImage = "url('/IMG/ombre-blue.svg')";
+    // document.getElementsByClassName('video-container')[0].style.display = 'none';
     document.getElementsByClassName('glass-container')[0].style.display = 'flex';
     document.getElementsByClassName('navbar')[0].style.display = 'flex';
     // document.getElementsByClassName('navbar')[1].style.display = 'inline-block';
     // document.getElementsByClassName('button-container').style.display = 'inline-block';
     document.getElementsByClassName('search-container')[0].style.display = 'flex';
     document.getElementsByClassName('link-bar')[0].style.display = 'flex';
-}, 3000);
+}, 5000);
 // the body element is initially set to have a background image of the loading page using the background-image property in CSS. The transition property is also used to add a smooth transition effect when the background image changes.
 //
 //     JavaScript is used to trigger the image change after an 8-second delay using the setTimeout function. The setTimeout function takes a callback function as its first argument, which sets the backgroundImage property of the body element to the fake data image after 8 seconds, effectively changing the background image.
