16fb5217cd97d8158b42d5727ed7e1993f281afa
Brittney Brinson <brittneyc95@outlook.com>
2023-04-15 13:37:18 -0400

    Added more components for the content to be housed in.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 setTimeout(function() {
     document.body.style.backgroundImage = "url('/IMG/ombre-blue.svg')";
-    // document.getElementsByClassName('video-container')[0].style.display = 'none';
     document.getElementsByClassName('glass-container')[0].style.display = 'flex';
+    document.getElementsByClassName('glass-container-delta')[0].style.display = 'flex';
     document.getElementsByClassName('navbar')[0].style.display = 'flex';
-    // document.getElementsByClassName('navbar')[1].style.display = 'inline-block';
-    // document.getElementsByClassName('button-container').style.display = 'inline-block';
     document.getElementsByClassName('search-container')[0].style.display = 'flex';
     document.getElementsByClassName('link-bar')[0].style.display = 'flex';
 }, 5000);
