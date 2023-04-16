367bc1f1292dc9425d45e51438914b3a71228638
Brittney Brinson <brittneyc95@outlook.com>
2023-04-16 18:27:50 -0400

    Added files

--- js/index.js
+++ js/index.js
@@ -5,7 +5,16 @@ setTimeout(function() {
     document.getElementsByClassName('navbar')[0].style.display = 'flex';
     document.getElementsByClassName('search-container')[0].style.display = 'inline-block';
     document.getElementsByClassName('link-bar')[0].style.display = 'flex';
-}, 3000);
+}, 2000);
+
+// get the users input from the search bar
+// then use that input to make an ajax call to the api
+// then display the data on the page
+
+
+
+
+
 // the body element is initially set to have a background image of the loading page using the background-image property in CSS. The transition property is also used to add a smooth transition effect when the background image changes.
 //
 //     JavaScript is used to trigger the image change after an 8-second delay using the setTimeout function. The setTimeout function takes a callback function as its first argument, which sets the backgroundImage property of the body element to the fake data image after 8 seconds, effectively changing the background image.
