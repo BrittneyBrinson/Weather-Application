4f5b2c690780cda9dfb1317701184dde096a8372
Brittney Brinson <brittneyc95@outlook.com>
2023-04-16 13:49:04 -0400

    Polished the links under the navbar, and now I'm working on a dropdown menu for locations that's connected to the users input

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@ setTimeout(function() {
     document.getElementsByClassName('glass-container')[0].style.display = 'flex';
     document.getElementsByClassName('glass-container-delta')[0].style.display = 'flex';
     document.getElementsByClassName('navbar')[0].style.display = 'flex';
-    document.getElementsByClassName('search-container')[0].style.display = 'flex';
+    document.getElementsByClassName('search-container')[0].style.display = 'inline-block';
     document.getElementsByClassName('link-bar')[0].style.display = 'flex';
-}, 5000);
+}, 3000);
 // the body element is initially set to have a background image of the loading page using the background-image property in CSS. The transition property is also used to add a smooth transition effect when the background image changes.
 //
 //     JavaScript is used to trigger the image change after an 8-second delay using the setTimeout function. The setTimeout function takes a callback function as its first argument, which sets the backgroundImage property of the body element to the fake data image after 8 seconds, effectively changing the background image.
