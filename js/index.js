70d3a1a08fecd1fd336ff9c0219df177549d03ef
Brittney Brinson <brittneyc95@outlook.com>
2023-05-09 23:12:43 -0400

    Trying to get the autocomplete sorted

--- js/index.js
+++ js/index.js
@@ -7,32 +7,59 @@ setTimeout(function() {
     document.getElementsByClassName('link-bar')[0].style.display = 'flex';
 }, 3000);
 
+var combobox = document.getElementById('combobox');
+var listbox = document.getElementById('listbox');
+
+combobox.addEventListener('input', function() {
+    var input = this.value;
+    var options = listbox.getElementsByTagName('li');
+    for (var i = 0; i < options.length; i++) {
+        var option = options[i];
+
+        if(option.textContent.toLowerCase().indexOf(input.toLowerCase()) !== -1){
+            option.style.display = 'block';
+        }
+        else {
+            option.style.display = 'none';
+        }
+    }
+    listbox.setAttribute('aria-activeDescendant', '');
+});
+listbox.addEventListener('click', function(e) {
+    if(e.target && e.target.nodeName === 'LI') {
+        combobox.value = e.target.textContent;
+        listbox.style.display = 'none';
+        listbox.setAttribute('aria-activeDescendant', '');
+    }
+});
 
 
 
 
 
-// the body element is initially set to have a background image of the loading page using the background-image property in CSS. The transition property is also used to add a smooth transition effect when the background image changes.
-//     JavaScript is used to trigger the image change after an 8-second delay using the setTimeout function. The setTimeout function takes a callback function as its first argument, which sets the backgroundImage property of the body element to the fake data image after 8 seconds, effectively changing the background image.
+
+
+
+
+
 //Next I need to use AJAX to make the call(request) to the API and get the data. I will use the jQuery library to make the AJAX call.
-let autocomplete;
-function initAutocomplete() {
-    autocomplete = new google.maps.places.Autocomplete(
-        document.getElementById('autocomplete'),
-        {
-            types: ['cities'],
-            componentRestrictions: { country: 'US' },
-        });
-}
-initAutocomplete();
+// let autocomplete;
+// function initAutocomplete() {
+//     autocomplete = new google.maps.places.Autocomplete(
+//         document.getElementById('autocomplete'),
+//         {
+//             types: ['cities'],
+//             componentRestrictions: { country: 'US' },
+//         });
+// }
+// initAutocomplete();
 
 // Get the input field id #autocomplete and use conditional logic to check if the user has entered a value. If the user has entered a value, the value is stored in the variable city. If the user has not entered a value, the variable city is set to the default value of 'New York'.
-let citySearch = document.getElementById("autocomplete"); //user input
-citySearch.addEventListener('keypress', function() { //keypress is used or keydown because it is triggered when the user presses a key on the keyboard and isn't triggered when the user clicks on the input field. A character must be entered for the event to be triggered.
-//attach the input element value to the select element, so that the user can select the city from the dropdown menu and the weather data will be displayed for the selected city.
-    let city = citySearch.value;
-    let dropdown = document.getElementById("dropdown");
-    dropdown.innerText = city;
-    //need to attach the results to a container aka a div under the input field
-});
+// let citySearch = document.getElementById("autocomplete"); //user input
+// citySearch.addEventListener('keypress', function() {
+//     let city = citySearch.value;
+//     let dropdown = document.getElementById("dropdown");
+//     dropdown.innerText = city;
+//     //need to attach the results to a container aka a div under the input field
+// });
 
