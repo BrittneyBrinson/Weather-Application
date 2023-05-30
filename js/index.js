64a3b75f0c3177c44554b290fd547908163b5150
Brittney Brinson <brittneyc95@outlook.com>
2023-05-30 18:06:47 -0400

    Got the data from the weather API pulling correctly.Now I'm inputing data into various places

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-setTimeout(function() {
+setTimeout(function () {
     document.body.style.backgroundImage = "url('/IMG/ombre-blue.svg')";
     document.getElementsByClassName('glass-container')[0].style.display = 'flex';
     document.getElementsByClassName('glass-container-delta')[0].style.display = 'flex';
@@ -8,24 +8,54 @@ setTimeout(function() {
 }, 3000);
 
 
-function getUserInput(){
+function getUserInput() {
     let userInput = document.getElementById('search-box-api').value;
-    console.log(userInput)
-    let showLocation = document.getElementById('display-search');
-    showLocation.innerHTML = "Location: " + userInput;
+    let showLocation = document.getElementById('location-name');
+    showLocation.innerHTML = "Location: " + userInput.toUpperCase();
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${userInput},&limit=5&appid=${weather_key}`)
         .then(response => response.json())
         .then(data => {
-            // Process the response data
-            // I need another fetch request using the long and lat from the first fetch request
+            let lat = data[0].lat;
+            let lon = data[0].lon;
+            return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${weather_key}`)
+        })
+        .then(response  => response.json())
+        .then((data) => {
+            console.log(data)
+            let temp = data.main.temp;
+            let tempF = Math.round((temp));
+            let showTemp = document.getElementById('temp');
 
-            console.log(data[0].lat)
-            console.log(data[0].lon);
-    })
+            let cloudyOrNot = data.weather[0].main;
+            let showCloudyOrNot = document.getElementById('cloudy-or-not');
+
+            let highTemp = data.main.temp_max;
+            let highTempF = Math.round((highTemp));
+            let showHighTemp = document.getElementById('high-temp');
+
+            let lowTemp = data.main.temp_min;
+            let lowTempF = Math.round((lowTemp));
+            let showLowTemp = document.getElementById('low-temp');
+
+
+            showTemp.innerHTML = "Currently: " + tempF + "°F";
+            showCloudyOrNot.innerHTML = cloudyOrNot;
+            showHighTemp.innerHTML = "High: " + highTempF + "°F";
+            showLowTemp.innerHTML = "Low: " + lowTempF + "°F";
+
+
+            let weatherBackground = document.getElementById('weather-background');
+            //section for if statements to change background image and icon based on weather
+            if (cloudyOrNot === "Clouds"){
+                weatherBackground.style.backgroundImage = "url('/IMG/cloudy.svg')";
+                weatherBackground.style.backgroundSize = "cover";
+            }
+        })
         .catch(error => {
             // Handle any errors
             console.error(error);
         });
+
 
 }
 
